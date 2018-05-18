a6697b40fae382d3b9abd4aad3601d8db4d46700
Christopher Monk <chris@abstractmonk.com>
2018-05-18 13:56:20 -0700

    add printCard method to TestRunner to test getting from the deck

--- TestRunner.java
+++ TestRunner.java
@@ -6,10 +6,17 @@ public class TestRunner {
         // create new deck and test it has 52 cards
         Deck deck1 = new Deck();
         printSize(deck1);
+
+        // get a card from the deck and print it
+        printCard(deck1);
     }
 
     public static void printSize(Deck d) {
         System.out.println(d.getSize());
     }
-
+    
+    public static void printCard(Deck d) {
+        Card c = d.getCard();
+        System.out.println(c);
+    }
 }

dcbd835a5ac152f0cdb513fdb4e89d001db9526f
Christopher Monk <chris@abstractmonk.com>
2018-05-18 15:02:19 -0700

    added some verbage to print statements for clarity, removed duplicate print stmt

--- TestRunner.java
+++ TestRunner.java
@@ -10,6 +10,10 @@ public class TestRunner {
         // get a card from top of the deck and print it
         printCard(deck1);
 
+        // shuffle deck1 and printCard
+        deck1.shuffle();
+        printCard(deck1);
+
         // draw card from top of deck and print it showing size of deck
         // before and after drawing occurs
         drawAndPrintCard(deck1);
@@ -17,20 +21,19 @@ public class TestRunner {
     }
 
     public static void printSize(Deck d) {
-        System.out.println(d.getSize());
+        System.out.println("Size of deck is: " + d.getSize());
     }
     
     public static void printCard(Deck d) {
         Card c = d.getCard();
-        System.out.println(c);
-        printSize(d);
+        System.out.println("The " + c + "is the next card in the deck");
     }
 
     public static void drawAndPrintCard(Deck d) {
         Card c;
         printSize(d);
         c = d.drawCard();
-        System.out.println(c);
+        System.out.println("The " + c + " was drawn.");
         printSize(d);
     }
 }

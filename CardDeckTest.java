9bae788fec834068debbe755cbb2f06edd419876
Christopher Monk <chris@abstractmonk.com>
2018-05-18 10:15:02 -0700

    wrote outlines for each test in comments above each test

--- CardDeckTest.java
+++ CardDeckTest.java
@@ -19,11 +19,23 @@ public class CardDeckTest {
     deck1 = new Deck();
     deck2 = new Deck(cards);
 
+    /* 
+     Create deck1 as full deck, and deck2 as empty deck. Make deep copy of 
+     full deck to empty deck. Shuffle the first deck. Assert deck1 not equal
+     to deck2 after shuffle.
+     Note: since shuffle is random, it's possible for this to fail occasionally.
+    */
     @Test
     public void shuffleTest() {
         
     }
     
+    /*
+     Create a full deck of cards. Create card variable to hold drawn cards.
+     Create count variable. Set count equal to number of cards in the deck.
+     Draw enough cards to empty deck. Attempt to draw card from an empty deck.
+     Assert card variable equals null.
+    */
     @Test
     public void drawAllCardsTest() {
         

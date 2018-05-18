50d695405021ee29f995bf893fd86f4eef8be13c
Christopher Monk <chris@abstractmonk.com>
2018-05-18 10:30:20 -0700

    added enums for Rank and Suit in Card class

--- Card.java
+++ Card.java
@@ -2,5 +2,14 @@
 // Card class represents an instance of a single card in a Deck
 
 public class Card {
+    
+    public enum Rank { 
+        ACE, KING, QUEEN, JACK, TEN, NINE, EIGHT,
+        SEVEN, SIX, FIVE, FOUR, THREE, TWO 
+    }
+
+    public enum Suit {
+        HEARTS, DIAMOND, CLUBS, SPADES
+    }
 
 }

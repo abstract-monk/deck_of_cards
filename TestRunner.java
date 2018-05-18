389dcfa016fe416cba5c3d0f9539b85eafe64e43
Christopher Monk <chris@abstractmonk.com>
2018-05-18 13:03:21 -0700

    testing creating new deck, and checking it has 52 cards

--- TestRunner.java
+++ TestRunner.java
@@ -1,7 +1,13 @@
 
 public class TestRunner {
-
+    
     public static void main(String[] args) {
+        
+        // create new deck and test it has 52 cards
+        Deck deck1 = new Deck();
+        int  size;
+        size = deck1.getSize();
+        System.out.println(size);
 
     }
 

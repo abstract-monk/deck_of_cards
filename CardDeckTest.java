a803dca17aa5ed28b4cbef73bf45e29c1fb33187
Christopher Monk <chris@abstractmonk.com>
2018-05-18 08:51:13 -0700

    add stub for unit test class

---
+++ CardDeckTest.java
@@ -0,0 +1,12 @@
+
+// Test Class to test functions for shuffling a deck of cards and drawing 
+// cards from a deck one at a time until the deck is empty.
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardDeckTest {
+
+
+}

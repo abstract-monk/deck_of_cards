59b2c1b61586b03e4c09f03d45a64ef8aeb3b421
Christopher Monk <chris@abstractmonk.com>
2018-05-18 09:30:13 -0700

    added List import, added cards field, changed init to add 2 decks of cards, one full, one empty

--- CardDeckTest.java
+++ CardDeckTest.java
@@ -2,22 +2,26 @@
 // Test Class to test functions for shuffling a deck of cards and drawing 
 // cards from a deck one at a time until the deck is empty.
 
+import java.util.List;
 import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 
 public class CardDeckTest {
 
-    private Deck deck1;
-    private Deck deck2;
-    private Card card;
+    private List<Card>      cards;
+    private Deck            deck1;
+    private Deck            deck2;
+    private Card            card;
 
     @Before
-    deck1 = new Deck()
+    cards = new ArrayList<>(Card);
+    deck1 = new Deck();
+    deck2 = new Deck(cards);
 
     @Test
     public void shuffleTest() {
-         
+        
     }
 
 }
